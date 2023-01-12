b391cf0572eddad93e5eed47c7b8eec3b5adcc54
Andentu <severalmaz97@gmail.com>
2023-01-12 11:51:06 +0300

    Итоговое задание модуля номер 3

--- Module_3.7/Program.cs
+++ Module_3.7/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name: ");
+            var PersonName = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Your name is {PersonName} and age is {age}");
+            Console.Write("\nEnter your birthdate: ");
+            var birthday = Console.ReadLine();
+            Console.WriteLine($"Your birthdate is {birthday}");
+
+            Console.WriteLine("Press enter...");
+            Console.ReadKey();
+
+
         }
     }
 }
