4ee0e94d13311713a3484b6fdabb5252b0bf5003
Andentu <severalmaz97@gmail.com>
2023-01-12 11:51:06 +0300

    Создан код итогового задания модуля 3

--- Module_3.7/Program.cs
+++ Module_3.7/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Здравствуйте!");
+            Console.Write("Введите ваше имя: ");
+            var PersonName = Console.ReadLine();
+            Console.Write("Укажите, сколько вам лет: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите вашу дату рождения в формате 01.01.2000 г.");
+            var birthday = Console.ReadLine();
+            Console.WriteLine("Вас зовут {0}. Вы родились {1} и на данный момент вам {2} лет.", PersonName, birthday, age);
+
+            Console.WriteLine(" Press enter...");
+            Console.ReadKey();
         }
     }
 }
